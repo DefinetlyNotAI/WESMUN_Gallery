1dc4067ba857ec824c9493b2068b2b6352739ba5
Shahm Najeeb <Shahm_Najeeb@outlook.com>
2026-01-19 21:38:54 +0400

    add icons to metadata for improved branding in gallery layout
    Signed-off-by: Shahm Najeeb <Shahm_Najeeb@outlook.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,12 @@ import {Metadata} from "next";
 export const metadata: Metadata = {
     title: 'WESMUN Gallery',
     description: 'A simple gallery for WESMUN images',
+    icons: {
+        icon: [
+            {url: '/wesmun.svg', type: 'image/svg+xml'},
+            {url: '/wesmun.webp', type: 'image/webp'},
+        ]
+    }
 }
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
