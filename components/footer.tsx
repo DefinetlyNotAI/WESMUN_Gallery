980be11487363b445db097cbbdf97b173930089c
Shahm Najeeb <Shahm_Najeeb@outlook.com>
2026-01-19 21:25:10 +0400

    update Footer component structure for improved layout and add visual separation
    Signed-off-by: Shahm Najeeb <Shahm_Najeeb@outlook.com>

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,11 +2,14 @@ import DeploymentInfo from "@/components/deployement-info";
 
 export function Footer() {
     return (
-        <footer className="bg-card border-t border-border">
-            <div className="mt-8 pt-8 border-t border-border text-center text-sm text-muted-foreground">
-                <p>&copy; {new Date().getFullYear()} WESMUN. All rights reserved.</p>
-                <DeploymentInfo/>
-            </div>
-        </footer>
+        <>
+            <footer className="bg-card border-t border-border">
+                <div className="mt-8 pt-8 text-center text-sm text-muted-foreground">
+                    <p>&copy; {new Date().getFullYear()} WESMUN. All rights reserved.</p>
+                    <DeploymentInfo/>
+                </div>
+            </footer>
+            <br className="border-t border-border"/>
+        </>
     )
 }
